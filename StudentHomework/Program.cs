82c25145db46687d7ea17a484349bee10c59f0d2
Alexandru Stoicea <137204266+alexandrustoicea@users.noreply.github.com>
2023-07-16 15:35:33 +0300

    Incarcat tema

---
+++ StudentHomework/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace StudentHomework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your full name: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Enter your age: ");
+            int age = Int32.Parse(Console.ReadLine());
+
+            Student student = new Student(name, age);
+
+            Console.WriteLine(student.Info);
+        }
+    }
+}
---
+++ StudentHomework/Student.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentHomework
+{
+    class Student
+    {
+        string _fullname;
+        int age;
+        int MIN_AGE = 18;
+        int MAX_AGE = 99;
+
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+
+            set
+            {
+                if (value < MIN_AGE || value > MAX_AGE)
+                {
+                    throw new ArgumentOutOfRangeException("This person doesn't have a valid age to be a student!");
+                    //Console.WriteLine("The age is not valid for being a student.");
+                }
+                else age = value;
+            }
+        }
+
+        public int? Mark { get; set; }
+
+        public Student(string fullname, int age)
+        {
+            _fullname = fullname;
+            Age = age;
+        }
+
+        public string Info
+        {
+            get
+            {
+                return "Student " + _fullname + " is " + age + " years old.";
+            }
+        }
+    }
+}
